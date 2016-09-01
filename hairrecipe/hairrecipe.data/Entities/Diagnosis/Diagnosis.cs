59f4830d81c80c56135d41d53810ce171a8decd0
unknown <calac01@WSMNL1CALAC02.ap.media.global.loc>
2016-09-01 14:18:40 +0800

    PG-292 [ refractor diagnosis model class ]

---
+++ hairrecipe/hairrecipe.data/Entities/Diagnosis/Diagnosis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hairrecipe.data.Entities.Diagnosis
+{
+    public class Diagnosis
+    {
+        public string QuestionsPartial { get; set; }
+        public string PagesPartial { get; set; }
+    }
+}
