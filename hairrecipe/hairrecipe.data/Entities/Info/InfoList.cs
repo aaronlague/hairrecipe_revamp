f9b5932ef8885a30bf8bcd75e6f6447a377f5716
Christian Amaro <xtian.amaro@gmail.com>
2016-09-07 16:49:14 +0800

    resolved conflict

--- hairrecipe/hairrecipe.data/Entities/Info/InfoList.cs
+++ hairrecipe/hairrecipe.data/Entities/Info/InfoList.cs
@@ -16,6 +16,8 @@ namespace hairrecipe.data.Entities.Info
         public string InfoNews2 { get; set; }
         public string InfoClass { get; set; }
         public string InfoLink { get; set; }
+        public string GACA { get; set; }
+        public string alt { get; set; }
         public string InfoImgAlt { get; set; }
     }
 }
