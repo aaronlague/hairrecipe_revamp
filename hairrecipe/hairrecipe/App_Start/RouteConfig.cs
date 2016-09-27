b833d31cf98010930e0cf885085126764b7f6358
unknown <calac01@WSMNL1CALAC02.ap.media.global.loc>
2016-09-27 10:05:24 +0800

    Convert to 4.0

--- hairrecipe/hairrecipe/App_Start/RouteConfig.cs
+++ hairrecipe/hairrecipe/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace hairrecipe
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.LowercaseUrls = true;
+            //routes.LowercaseUrls = true;
 
             routes.MapRoute(
                 name: "Default",
