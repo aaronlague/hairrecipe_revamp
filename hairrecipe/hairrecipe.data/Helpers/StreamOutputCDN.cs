cbab696712643c662659d136e1ee89a246de5382
unknown <calac01@WSMNL1CALAC02.ap.media.global.loc>
2016-09-30 17:15:36 +0800

    Angelica Revamp - CDN Implementation

---
+++ hairrecipe/hairrecipe.data/Helpers/StreamOutputCDN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace hairrecipe.data.Helpers.PageFilter
+{
+    public class StreamOutputCDN : IHttpModule
+    {
+
+        //-----------------------------------------------------------------------------------------
+        public void Init (HttpApplication app)
+        {
+            app.ReleaseRequestState         += new EventHandler(InstallResponseFilter);
+        }
+
+
+        /// <summary>
+        /// Use this event to wire a page filter.
+        /// </summary>
+        private void InstallResponseFilter(object sender, EventArgs e) 
+        {
+            HttpResponse response = HttpContext.Current.Response;
+
+            if(response.ContentType == "text/html")
+                response.Filter = new StreamFilter(response.Filter);      
+        }
+        public void Dispose() { }
+    }
+}
+
