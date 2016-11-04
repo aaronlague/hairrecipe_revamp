0a5e222d7c88a104e80bd550eaa4c896cbe7a187
aaronlague <aaronlague@yahoo.com>
2016-11-04 15:28:39 +1100

    post live fixes - slide 2, 4, 5 and 6

---
+++ hairrecipe/hairrecipe/Content/js/overrides-sp.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    console.log(window.location.pathname);
+    $('.navbar-toggle.nav-burger').click(function () {
+        $(this).addClass('hide-opacity');
+        $(this).removeClass('show-opacity');
+    });
+
+    $('.navbar-toggle.nav-close').click(function () {
+        $('.navbar-toggle.nav-burger').removeClass('hide-opacity');
+        $('.navbar-toggle.nav-burger').addClass('show-opacity');
+    });
+
+    $('.navbar-toggle.nav-slide-right').click(function () {
+        $('.navbar-toggle.nav-burger').css('display', 'block');
+        $('.navbar-toggle.nav-burger').addClass('show-opacity');
+    });
+
+    if (window.location.pathname == "/sp/info") {
+        var customImgAlt = $("a[binid^='custom-hawaii-hp']").find('img');
+        customImgAlt.attr('alt', 'ヘアレシピ ビューティーキャンプ');
+    }
+
+    if ((window.location.pathname == "/sp") || (window.location.pathname == "/sp/info")) {
+        $("a[binid^='custom-cosme-hp']").attr('target', '_blank');
+    }
+})
